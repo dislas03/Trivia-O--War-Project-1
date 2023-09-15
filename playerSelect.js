8296fbbda82d63f74a66cb13ddb554ff4ce5d3c5
dislas03 <islas0903@gmail.com>
2023-09-15 14:57:11 -0700

    working on code for player selection

--- playerSelect.js
+++ playerSelect.js
@@ -1,6 +1,46 @@
-//Players should be able to click and type into the text box 
-//Players names should be saved into text box (maybe stored into an array? or perhaps an object?)
-//Have "computer" select who starts the game first 
-// have a class that can represent any player that plays the game? 
-//add functionality to the button which will open and close the hidden text box 
+let playerOne = document.querySelector("#playerOne");
 
+let playerTwo = document.querySelector("#playerTwo");
+
+let playerArray = [playerOne, playerTwo];
+
+let randomPlayer = playerArray[Math.floor(Math.random() * playerArray.length)];
+
+let selectPlayer = document.querySelector("#selectPlayer");
+
+const playerOneButton = document.querySelector("#playerOneEnter");
+
+const playerTwoButton = document.querySelector("#playerTwoEnter");
+
+const howToPlayModal = document.querySelector("#howToPlay");
+
+const openHowToButton = document.querySelector("#howTo");
+
+const closeBackToGame = document.querySelector("#close");
+
+const startGame = document.querySelector("#startGame"); 
+
+playerOneButton.addEventListener("click", () =>{
+    playerOne.innerHTML = playerOne.value
+});
+
+playerTwoButton.addEventListener("click", () =>{
+    playerTwo.innerHTML = playerTwo.value
+});
+
+openHowToButton.addEventListener("click", () =>{
+    howToPlayModal.style.display = "block"
+}); 
+
+closeBackToGame.addEventListener("click", () =>{
+    howToPlayModal.style.display = "none"
+});
+
+startGame.addEventListener("click", () =>{
+    window.location.href = "playGame.html";
+}); 
+
+selectPlayer.addEventListener("click", () =>{
+    let playerSelected = document.querySelector(".playerSelected"); 
+    playerSelected.innerHTML = randomPlayer
+}); 
