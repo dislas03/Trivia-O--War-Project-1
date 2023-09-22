fed7a07cceb88ac2fffbbb94a0379004dd355a51
dislas03 <islas0903@gmail.com>
2023-09-22 16:08:16 -0700

    score counter is working

--- playGame.js
+++ playGame.js
@@ -50,9 +50,9 @@ const questionNine = document.querySelector(".qNineModal");
 
 const questionTen = document.querySelector(".qTenModal"); 
 
-const clearStorage = document.querySelector("#clear");
+const clearStorageModal = document.querySelector(".clearLocalStorage");
 
-// let selectedAnswer = document.querySelectorAll(".selectAnswer") 
+const clearStorage = document.querySelector("#clear");
 
 let selectedAnswerOne = document.querySelectorAll(".selectAnswerOne");
 
@@ -68,18 +68,13 @@ let selectedAnswerFour = document.querySelectorAll(".selectAnswerFour");
 
 let selectedAnswerFive = document.querySelectorAll(".selectAnswerFive");
 
-
 let selectedAnswerSix = document.querySelectorAll(".selectAnswerSix");
-
 
 let selectedAnswerSeven = document.querySelectorAll(".selectAnswerSeven");
 
-
 let selectedAnswerEight = document.querySelectorAll(".selectAnswerEight");
 
-
 let selectedAnswerNine = document.querySelectorAll(".selectAnswerNine")
-
 
 let selectedAnswerTen = document.querySelectorAll(".selectAnswerTen");
 
@@ -87,11 +82,11 @@ let correctAnswer = document.querySelector(".correctAnswerModal");
 
 let incorrectAnswer = document.querySelector(".incorrectAnswerModal"); 
 
-let nextTurn = document.querySelector(".nextTurn");
+let nextTurn = document.querySelectorAll(".nextTurn")
 
 let playerOneScore = document.querySelector(".playerOneScore");
 
-playerOneScore = 0;
+let playerScore = 0;
 
 // let playerTwoScore = 0 
 
@@ -157,7 +152,19 @@ console.log(questionsArray);
 //     console.log(playerTwoName)
 // }
 
-takeTurnButton.addEventListener("click", () => {
+let scoreIncrement = (amt) => {
+    playerScore += amt;
+    playerOneScore.innerHTML = playerScore;
+    return amt;
+}
+
+let scoreDecrement = (amt) => {
+    playerScore -= amt;
+    playerOneScore.innerHTML = playerScore;
+    return amt;
+}
+
+takeTurn = takeTurnButton.addEventListener("click", () => {
     questionsModal.style.display = "block"
     buttonClass.style.display = "none"
     let randomQuestion = questionsArray[Math.floor(Math.random() * questionsArray.length)];
@@ -165,194 +172,228 @@ takeTurnButton.addEventListener("click", () => {
     // playerStart.style.display = "none"
 });
 
-const turn = nextTurn.addEventListener("click", () => {
-    for (i = 0; i < questionsModal.length; i ++){
-        if (questionsModal[i].display.style === "block"){
-            questionsModal[i].display.style = "none"
+nextTurn.forEach(nextTurn => {
+    nextTurn.addEventListener("click", () => {
+        for (i = 0; i < questionsModal.length; i++){
+            if (questionsModal[i].style.display === "block"){
+                questionsModal[i].style.display = "none"
+            }
         }
-    }
-    let randomQuestion = questionsArray[Math.floor(Math.random() * questionsArray.length)];
-    randomQuestion.question.style.display = "block"
+        let randomQuestion = questionsArray[Math.floor(Math.random() * questionsArray.length)];
+        randomQuestion.question.style.display = "block"
+    })
 })
+// const correctTurn = correctNextTurn.addEventListener("click", () => {
+//     questionsModal.style.display = "block"
+//     let randomQuestion = questionsArray[Math.floor(Math.random() * questionsArray.length)];
+//         randomQuestion.question.style.dipslay = "block"
+// })
+
+// const incorrectTurn = incorrectNextTurn.addEventListener("click", () => {
+//     questionsModal.style.display = "block"
+//     let randomQuestion = questionsArray[Math.floor(Math.random() * questionsArray.length)];
+//         randomQuestion.question.style.dipslay = "block"
+// })
 
 selectedAnswerOne.forEach(selectAnswerOne => {
     selectAnswerOne.addEventListener("click", () => {
-    for(i = 0; i < selectedAnswerOne.length; i++)
-    {
-    if (selectAnswerOne.innerText === questionsArray[0].correctAnswer){
-    correctAnswer.style.display = "block"
-    playerOneScore += 2
-    turn
-    } else{
-    incorrectAnswer.style.display = "block"
-    playerOneScore -= 1
-    turn
-    }
-    }
+        for(i = 0; i < selectedAnswerOne.length; i++){
+            if (selectAnswerOne.innerText === questionsArray[0].correctAnswer){
+                correctAnswer.style.display = "block"
+                scoreIncrement(2)
+                takeTurn
+            } else{
+                incorrectAnswer.style.display = "block"
+                scoreDecrement(1)
+                takeTurn
+            }
+        }
+        if (playerScore >= 80){
+            clearStorageModal.style.display = "block"
+        }
     })
-    })
+})
     
     
-    selectedAnswerTwo.forEach(selectAnswerTwo => {
+selectedAnswerTwo.forEach(selectAnswerTwo => {
     selectAnswerTwo.addEventListener("click", () =>{
-    for (i = 0; i < selectedAnswerTwo.length; i++)
-    {
-    if (selectAnswerTwo.innerText === questionsArray[1].correctAnswer){
-    correctAnswer.style.display = "block"
-    playerOneScore += 2
-    turn
-    } else{
-    incorrectAnswer.style.display = "block"
-    playerOneScore -= 1
-    turn
-    }
-    }
+        for (i = 0; i < selectedAnswerTwo.length; i++){
+            if (selectAnswerTwo.innerText === questionsArray[1].correctAnswer){
+                correctAnswer.style.display = "block"
+                scoreIncrement(2)
+                takeTurn
+            } else{
+                incorrectAnswer.style.display = "block"
+                scoreDecrement(1)
+                takeTurn
+            }
+        }
+        if (playerScore >= 80){
+            clearStorageModal.style.display = "block"
+        }
     })
-    })
+})
     
     
-    selectedAnswerThree.forEach(selectAnswerThree => {
+selectedAnswerThree.forEach(selectAnswerThree => {
     selectAnswerThree.addEventListener("click", () => {
-    for (i = 0; i < selectedAnswerThree.length; i++)
-    {
-    if (selectAnswerThree.innerText === questionsArray[2].correctAnswer){
-    correctAnswer.style.display = "block"
-    playerOneScore += 2
-    turn
-    } else{
-    incorrectAnswer.style.display = "block"
-    playerOneScore -= 1
-    turn
-    }
-    }
+        for (i = 0; i < selectedAnswerThree.length; i++){
+            if (selectAnswerThree.innerText === questionsArray[2].correctAnswer){
+                correctAnswer.style.display = "block"
+                scoreIncrement(2)
+                takeTurn
+            } else{
+                incorrectAnswer.style.display = "block"
+                scoreDecrement(1)
+                takeTurn
+            }
+        }
+        if (playerScore >= 80){
+            clearStorageModal.style.display = "block"
+        }
     })
-    })
+})
     
     
-    selectedAnswerFour.forEach(selectAnswerFour => {
+selectedAnswerFour.forEach(selectAnswerFour => {
     selectAnswerFour.addEventListener("click", () => {
-    for (i = 0; i < selectedAnswerFour.length; i++)
-    {
-    if (selectAnswerFour.innerText === questionsArray[3].correctAnswer){
-    correctAnswer.style.display = "block"
-    playerOneScore += 2
-    turn
-    } else{
-    incorrectAnswer.style.display = "block"
-    playerOneScore -= 1
-    turn
-    }
-    }
+        for (i = 0; i < selectedAnswerFour.length; i++){
+            if (selectAnswerFour.innerText === questionsArray[3].correctAnswer){
+                correctAnswer.style.display = "block"
+                scoreIncrement(2)
+                takeTurn
+            } else{
+                incorrectAnswer.style.display = "block"
+                scoreDecrement(1)
+                takeTurn
+            }
+        }
+        if (playerScore >= 80){
+            clearStorageModal.style.display = "block"
+        }
     })
-    })
+})
     
     
-    selectedAnswerFive.forEach(selectAnswerFive => {
+selectedAnswerFive.forEach(selectAnswerFive => {
     selectAnswerFive.addEventListener("click", () => {
-    for (i = 0; i < selectedAnswerFive.length; i++)
-    {
-    if (selectAnswerFive.innerText === questionsArray[4].correctAnswer){
-    correctAnswer.style.display = "block"
-    playerOneScore += 2
-    turn
-    } else{
-    incorrectAnswer.style.display = "block"
-    playerOneScore -= 1
-    turn
-    }
-    }
+        for (i = 0; i < selectedAnswerFive.length; i++){
+            if (selectAnswerFive.innerText === questionsArray[4].correctAnswer){
+                correctAnswer.style.display = "block"
+                scoreIncrement(2)
+                takeTurn
+            } else{
+                incorrectAnswer.style.display = "block"
+                scoreDecrement(1)
+                takeTurn
+            }
+        }
+        if (playerScore >= 80){
+            clearStorageModal.style.display = "block"
+        }
     })
-    })
+})
     
     
-    selectedAnswerSix.forEach(selectAnswerSix => {
+selectedAnswerSix.forEach(selectAnswerSix => {
     selectAnswerSix.addEventListener("click", () => {
-    for (i = 0; i < selectedAnswerSix.length; i++)
-    {
-    if (selectAnswerSix.innerText === questionsArray[5].correctAnswer){
-    correctAnswer.style.display = "block"
-    playerOneScore += 2
-    turn
-    } else{
-    incorrectAnswer.style.display = "block"
-    playerOneScore -= 1
-    turn
-    }
-    }
+        for (i = 0; i < selectedAnswerSix.length; i++){
+            if (selectAnswerSix.innerText === questionsArray[5].correctAnswer){
+                correctAnswer.style.display = "block"
+                scoreIncrement(2)
+                takeTurn
+            } else{
+                incorrectAnswer.style.display = "block"
+                scoreDecrement(1)
+                takeTurn
+            }
+        }
+        if (playerScore >= 80){
+            clearStorageModal.style.display = "block"
+        }
     })
-    })
+})
     
     
-    selectedAnswerSeven.forEach(selectAnswerSeven => {
+selectedAnswerSeven.forEach(selectAnswerSeven => {
     selectAnswerSeven.addEventListener("click", () => {
-    for (i = 0; i < selectedAnswerSeven.length; i++)
-    {
-    if (selectAnswerSeven.innerText === questionsArray[6].correctAnswer){
-    correctAnswer.style.display = "block" 
-    playerOneScore += 2
-    turn
-    } else{
-    incorrectAnswer.style.display = "block"
-    playerOneScore -= 1
-    turn
-    }
-    }
+        for (i = 0; i < selectedAnswerSeven.length; i++)
+        {
+            if (selectAnswerSeven.innerText === questionsArray[6].correctAnswer){
+                correctAnswer.style.display = "block" 
+                scoreIncrement(2)
+                takeTurn
+            } else{
+                incorrectAnswer.style.display = "block"
+                scoreDecrement(1)
+                takeTurn
+            }
+        }
+        if (playerScore >= 80){
+            clearStorageModal.style.display = "block"
+        }
     })
-    })
+})
     
     
-    selectedAnswerEight.forEach(selectAnswerEight => {
+selectedAnswerEight.forEach(selectAnswerEight => {
     selectAnswerEight.addEventListener("click", () => {
-    for (i = 0; i < selectedAnswerEight.length; i++)
-    {
-    if (selectAnswerEight.innerText === questionsArray[7].correctAnswer){
-    correctAnswer.style.display = "block"
-    playerOneScore += 2
-    turn
-    } else{
-    incorrectAnswer.style.display = "block"
-    playerOneScore -= 1
-    turn
-    }
-    }
+        for (i = 0; i < selectedAnswerEight.length; i++){
+            if (selectAnswerEight.innerText === questionsArray[7].correctAnswer){
+                correctAnswer.style.display = "block"
+                scoreIncrement(2)
+                takeTurn
+            } else{
+                incorrectAnswer.style.display = "block"
+                scoreDecrement(1)
+                takeTurn
+            }
+        }
+        if (playerScore >= 80){
+            clearStorageModal.style.display = "block"
+        }
     })
-    })
+})
     
     
-    selectedAnswerNine.forEach(selectAnswerNine => {
+selectedAnswerNine.forEach(selectAnswerNine => {
     selectAnswerNine.addEventListener("click", () => {
-    for (i = 0; i < selectedAnswerNine.length; i++)
-    {
-    if (selectAnswerNine.innerText === questionsArray[8].correctAnswer){
-    correctAnswer.style.display = "block"
-    playerOneScore += 2
-    turn
-    } else{
-    incorrectAnswer.style.display = "block"
-    playerOneScore -= 1
-    turn
-    }
-    }
+        for (i = 0; i < selectedAnswerNine.length; i++){
+            if (selectAnswerNine.innerText === questionsArray[8].correctAnswer){
+                correctAnswer.style.display = "block"
+                scoreIncrement(2)
+                takeTurn
+            } else{
+                incorrectAnswer.style.display = "block"
+                scoreDecrement(1)
+                takeTurn
+            }
+        }
+        if (playerScore >= 80){
+            clearStorageModal.style.display = "block"
+        }
     })
-    })
+})
     
     
-    selectedAnswerTen.forEach(selectAnswerTen => {
+selectedAnswerTen.forEach(selectAnswerTen => {
     selectAnswerTen.addEventListener("click", () => {
-    for (i = 0; i < selectedAnswerTen.length; i++)
-    {
-    if (selectAnswerTen.innerText === questionsArray[9].correctAnswer){
-    correctAnswer.style.display = "block"
-    playerOneScore += 2
-    turn
-    } else{
-    incorrectAnswer.style.display = "block"
-    playerOneScore -= 1
-    turn
-    }
-    }
+        for (i = 0; i < selectedAnswerTen.length; i++){
+            if (selectAnswerTen.innerText === questionsArray[9].correctAnswer){
+                correctAnswer.style.display = "block"
+                scoreIncrement(2)
+                takeTurn
+            } else{
+                incorrectAnswer.style.display = "block"
+                scoreDecrement(1)
+                takeTurn
+            }
+        }
+        if (playerScore >= 80){
+            clearStorageModal.style.display = "block"
+        }
     })
-    })
+})
     
 
 clearStorage.addEventListener("click", () =>{
